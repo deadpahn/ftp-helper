509f4879358a2aa1b06d2c86ab2eff266a3cbe09
foo <arf>
2023-04-16 14:58:29 -0400

    big clean up of logic

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+csvFile="files.csv"
+# make new files csv
+> $csvFile
+
 # Check for dependencies
 if ! command -v ncftpput &> /dev/null
 then
@@ -20,22 +24,36 @@ FTP_USERNAME=$(grep "ftp_username=" config.txt | cut -d= -f2)
 FTP_PASSWORD=$(grep "ftp_password=" config.txt | cut -d= -f2)
 
 #SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo "The script you are running has basename $( basename -- "$0"; ), dirname $( dirname -- "$0"; )";
-echo "The present working directory is $( pwd; )";
+#echo "The script you are running has basename $( basename -- "$0"; ), dirname $( dirname -- "$0"; )";
+#echo "The present working directory is $( pwd; )";
 
 # Set the folder path
 folder_path=$( pwd; )
+files=()
+while IFS= read -r -d '' file; do
+  files+=("$file" "${file//\"/\\\"}")
+done < <(find "$folder_path" -maxdepth 1 \( -type f -o -type d \) -print0)
 
+for (( i=0; i<${#files[@]}; i+=2 )); do
+    file="${files[i]}"
+    display_name="$(basename "${files[i+1]}")"
+    echo '"'${display_name}'"' '|' '"'${file}'"' >> files.csv
+done
 
-# Get a list of files in the folder
-files=$(ls $folder_path)
+# Add files from CSV to the array
+while IFS='|' read -r name path; do
+  filesForDialog+=("$name" "$path" off)
+done < "$csvFile"
 
+# Print the contents of the files array
+#printf '%s\n' "${filesForDialog[@]}"
+#exit
 # Create the dialog window using dialog
 selected_files=$(dialog --stdout \
                     --title "Select Files" \
                     --checklist "Choose files to upload:" \
                     0 0 0 \
-                    $(for file in $files; do echo "$file \"$file\" off"; done))
+                    "${filesForDialog[@]}")
 
 # Copy the selected items to the FTP site
 for file in $selected_files; do
